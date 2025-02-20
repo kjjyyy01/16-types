073e8f7e44d1dbdb59037a9f406bb376400a4095
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-20 19:54:03 +0900

    #1 - Structure: profile 페이지 기본 구조

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,13 @@
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <h1>프로필 수정</h1>
+      <form>
+        <input type="text" pattern="^{2,6}$" required />
+        <button>수정 완료</button>
+      </form>
+    </div>
+  );
 };
 
 export default Profile;
