69960f2cbb2d9e8ca30e6020ed960eb3fd4f824d
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-20 19:41:37 +0900

    #1 - Structure: signup 페이지 기본 구조

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div>
+      <h1>회원가입</h1>
+      <form>
+        <input type="text" pattern="^([a-z0-9]){6,50}$" required />
+        <input type="password" pattern="^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_-+=[]{}~?:;`|/]).{6,15}$" required />
+        <input type="text" pattern="^{2,6}$" required />
+        <button>회원가입</button>
+      </form>
+      <Link to={"/login"}>
+        <button>로그인하고 계속하세요!</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Signup;
