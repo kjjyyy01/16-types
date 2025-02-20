3cd124eca1c13061c3b57eb1310db406fbf7406a
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-20 16:48:50 +0900

    #1 - 페이지 생성

---
+++ src/shared/Router.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Test from "../pages/Test";
+import Results from "../pages/Results";
+import Profile from "../pages/Profile";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
