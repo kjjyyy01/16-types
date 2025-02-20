98dc529e4d014c34429b6a4ea2edab20a8feb8c9
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-20 20:16:59 +0900

    #1 - Structure: test, results 페이지 기본 구조

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,5 +1,20 @@
 const Results = () => {
-  return <div>Results</div>;
+  return (
+    <div>
+      <h1>모든 검사 결과</h1>
+      <ul>
+        <li>
+          <h2>ISTJ</h2>
+          <p>
+            ISTJ, Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem unde neque quisquam ad aut
+            odit vitae animi. Officia aut tempora fugiat placeat, eius, ea, atque cumque nemo veniam rerum consequatur.
+          </p>
+          <button>공개로 전환</button>
+          <button>삭제</button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Results;
