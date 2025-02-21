06921fc929242fa647417bf13ab2252cf6c31f76
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-21 21:58:39 +0900

    #1 - Style: 공통 컬러 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        header: "#DDBC89",
+        text: "#AA512F",
+        background: "#EDE7CF",
+        card: "#F9F3CF",
+        button: "#AA512F",
+        buttontext: "#EDE7CF",
+      },
+    },
   },
   plugins: [],
 };
