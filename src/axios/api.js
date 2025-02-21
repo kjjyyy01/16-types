99a71bafd4b2d4f6971795c59a0771be7981516e
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-21 15:10:39 +0900

    #1 - Structure: api.js, env파일 수정

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const authAPI = axios.create({
-  baseURL: import.meta.env.AUTH_SERVER_URL,
+export const authAPI = axios.create({
+  baseURL: import.meta.env.VITE_AUTH_SERVER_URL,
 });
-const jsonServerAPI = axios.create({
-  baseURL: import.meta.env.JSON_SERVER_URL,
+export const jsonServerAPI = axios.create({
+  baseURL: import.meta.env.VITE_JSON_SERVER_URL,
 });
-
-export default { authAPI, jsonServerAPI };
