ba5310d7e75a248682c010b503e79b4792005118
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-22 00:46:35 +0900

    #1 - Style: 스타일링

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,14 +1,14 @@
 const Test = () => {
   return (
     <div>
-      <h1>MBTI 유형 검사</h1>
-      <form>
+      <form className="flex flex-col justify-center items-center gap-14 mt-14">
+        <h1 className="text-5xl font-bold">MBTI 유형 검사</h1>
         <ul>
-          <li>
-            <p>데이트 중에도 GitHub를 체크하는 당신, 연인이 그런 당신을 이해할까요?</p>
+          <li className="flex flex-col items-start justify-center">
+            <p className="mb-1 font-semibold">데이트 중에도 GitHub를 체크하는 당신, 연인이 그런 당신을 이해할까요?</p>
             <label htmlFor="top-radio">
               <input type="radio" name="understanding" id="top-radio" />
-              이해할 것
+              <span>이해할 것</span>
             </label>
             <label htmlFor="bottom-radio">
               <input type="radio" name="understanding" id="bottom-radio" />
