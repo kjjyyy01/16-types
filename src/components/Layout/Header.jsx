4bf61a1f98a9cebae340156fe0e4eb43a8474034
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-21 21:54:51 +0900

    #1 - Structure: 헤더 레이아웃 작성

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,5 +1,19 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-header">
+      <form className="flex justify-between items-center">
+        <span>
+          <a href="/">
+            <img src="/src/assets/main-logo.png" alt="16타입 로고" className="h-20 m-4" />
+          </a>
+        </span>
+        <section>
+          <button className="mr-5 bg-inherit text-text">로그인</button>
+          <button className="mr-5 bg-inherit text-text">회원가입</button>
+        </section>
+      </form>
+    </div>
+  );
 };
 
 export default Header;
