f10145e873f27af42a55e93b8b5f2db8776c56c4
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-22 15:49:41 +0900

    #1 - Structure: signup페이지를 login페이지로 컴포넌트 통합

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [isLoginMode, setIsLoginMode] = useState(false);
+
   return (
     <div className="flex flex-col items-center justify-center">
       <main className="w-1/5 mt-20 bg-card flex flex-col items-center justify-center p-10 rounded-md shadow-[0_0_10px_#DDBC89]">
-        <form className="flex flex-col items-center justify-center gap-10">
-          <h1 className="text-5xl font-bold">로그인</h1>
+        <form className="flex flex-col items-center justify-center gap-10 w-3/5">
+          <h1 className="text-5xl font-bold">{isLoginMode ? "회원가입" : "로그인"}</h1>
           <input type="text" placeholder="아이디" pattern="^([a-z0-9]){6,50}$" required />
           <input
             type="password"
@@ -13,12 +15,13 @@ const Login = () => {
             pattern="^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_-+=[]{}~?:;`|/]).{6,15}$"
             required
           />
-          <button>로그인</button>
+          {isLoginMode && <input type="text" placeholder="닉네임" pattern="^{2,6}$" required />}
+          <button>{isLoginMode ? "회원가입" : "로그인"}</button>
         </form>
-        <p className="mt-7 text-base">아직 회원이 아니신가요?</p>
-        <Link to={"/signup"}>
-          <button className="bg-inherit text-text">회원가입</button>
-        </Link>
+        <p className="mt-7 text-base">{isLoginMode ? "이미 회원 이신가요?" : "아직 회원이 아니신가요?"}</p>
+        <button onClick={() => setIsLoginMode((prev) => !prev)} className="bg-inherit text-text">
+          {isLoginMode ? "로그인" : "회원가입"}
+        </button>
       </main>
     </div>
   );
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import Signup from "../pages/Signup";
 import Test from "../pages/Test";
 import Results from "../pages/Results";
 import Profile from "../pages/Profile";
@@ -11,7 +10,6 @@ const Router = () => {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
       <Route path="/test" element={<Test />} />
       <Route path="/results" element={<Results />} />
       <Route path="/profile" element={<Profile />} />
