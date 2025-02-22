303bf2f9c8675bd5775d476d9aadbbfc8c5875d3
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-22 21:30:42 +0900

    #2 - Feature: 로그인 여부에 따른 Protected Routing

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,18 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Test from "../pages/Test";
 import Results from "../pages/Results";
 import Profile from "../pages/Profile";
+import useBearsStore from "../zustand/bearsStore";
+
+const PrivateRoute = () => {
+  const { isLogin } = useBearsStore((state) => state);
+  return <>{isLogin ? <Outlet /> : <Navigate to="/login" />}</>;
+};
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/test" element={<Test />} />
-      <Route path="/results" element={<Results />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route element={<PrivateRoute />}>
+        <Route path="/test" element={<Test />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
     </Routes>
   );
 };
