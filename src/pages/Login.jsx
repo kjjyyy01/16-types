687f20129d76e7b898dabbaa80d5191dff6bb7e0
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-22 18:27:51 +0900

    #2 - 로그인,로그아웃 기능 구현 로컬스토리지에 access토큰 저장

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,26 +1,91 @@
 import { useState } from "react";
+import { authAPI } from "../axios/api";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [isLoginMode, setIsLoginMode] = useState(false);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [form, setForm] = useState({
+    id: "",
+    password: "",
+    nickname: "",
+  });
+  const navigate = useNavigate();
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const resetForm = () => {
+    setForm({
+      id: "",
+      password: "",
+      nickname: "",
+    });
+  };
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    if (isLoginMode) {
+      const { data } = await authAPI.post("/login", {
+        id: form.id,
+        password: form.password,
+      });
+      alert("로그인이 완료되었습니다.");
+      localStorage.setItem("accessToken", data.accessToken);
+      navigate("/");
+      resetForm();
+    } else {
+      await authAPI.post("/register", {
+        id: form.id,
+        password: form.password,
+        nickname: form.nickname,
+      });
+      alert("회원가입이 완료되었습니다.");
+      setIsLoginMode(true);
+      resetForm();
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center">
       <main className="w-1/5 mt-20 bg-card flex flex-col items-center justify-center p-10 rounded-md shadow-[0_0_10px_#DDBC89]">
-        <form className="flex flex-col items-center justify-center gap-10 w-3/5">
-          <h1 className="text-5xl font-bold">{isLoginMode ? "회원가입" : "로그인"}</h1>
-          <input type="text" placeholder="아이디" pattern="^([a-z0-9]){6,50}$" required />
+        <form onSubmit={onSubmitHandler} className="flex flex-col items-center justify-center gap-10 w-3/5">
+          <h1 className="text-5xl font-bold">{isLoginMode ? "로그인" : "회원가입"}</h1>
+          <input
+            type="text"
+            name="id"
+            value={form.id}
+            onChange={onChangeHandler}
+            placeholder="아이디"
+            pattern="^([a-z0-9]){6,50}$"
+            required
+          />
           <input
             type="password"
+            name="password"
+            value={form.password}
+            onChange={onChangeHandler}
             placeholder="비밀번호"
             pattern="^(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*()_-+=[]{}~?:;`|/]).{6,15}$"
             required
           />
-          {isLoginMode && <input type="text" placeholder="닉네임" pattern="^{2,6}$" required />}
-          <button>{isLoginMode ? "회원가입" : "로그인"}</button>
+          {!isLoginMode && (
+            <input
+              type="text"
+              name="nickname"
+              value={form.nickname}
+              onChange={onChangeHandler}
+              placeholder="닉네임"
+              pattern="^{2,6}$"
+              required
+            />
+          )}
+          <button>{isLoginMode ? "로그인" : "회원가입"}</button>
         </form>
-        <p className="mt-7 text-base">{isLoginMode ? "이미 회원 이신가요?" : "아직 회원이 아니신가요?"}</p>
-        <button onClick={() => setIsLoginMode((prev) => !prev)} className="bg-inherit text-text">
-          {isLoginMode ? "로그인" : "회원가입"}
+        <p className="mt-7 text-base">{isLoginMode ? "아직 회원이 아니신가요?" : "이미 회원 이신가요?"}</p>
+        <button type="button" onClick={() => setIsLoginMode((prev) => !prev)} className="bg-inherit text-text">
+          {isLoginMode ? "회원가입" : "로그인"}
         </button>
       </main>
     </div>
