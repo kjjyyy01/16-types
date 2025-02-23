25a8d530b1f5d7ed3030e3093d5329edfe1f32ca
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-23 14:09:02 +0900

    #13 - Structure: 테스트 데이터 브라우저 렌더링

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,22 +1,41 @@
+import { questions } from "../data/questions";
+
 const Test = () => {
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
-      <form className="flex flex-col justify-center items-center gap-14 mt-14">
+      <form onSubmit={onSubmitHandler} className="flex flex-col justify-center items-center gap-10 mt-14">
         <h1 className="text-5xl font-bold">MBTI 유형 검사</h1>
-        <ul>
-          <li className="flex flex-col items-start justify-center">
-            <p className="mb-1 font-semibold">데이트 중에도 GitHub를 체크하는 당신, 연인이 그런 당신을 이해할까요?</p>
-            <label htmlFor="top-radio">
-              <input type="radio" name="understanding" id="top-radio" />
-              <span>이해할 것</span>
-            </label>
-            <label htmlFor="bottom-radio">
-              <input type="radio" name="understanding" id="bottom-radio" />
-              이해하지 못할 것
-            </label>
-          </li>
+        <ul className="w-2/5">
+          {questions.map((question) => {
+            return (
+              <li key={question.id} className="flex flex-col mb-5">
+                <p className="font-semibold">{question.question}</p>
+                <label htmlFor={`top-radio-${question.id}`}>
+                  <input
+                    type="radio"
+                    name={`answer-${question.id}`}
+                    id={`top-radio-${question.id}`}
+                    className="w-auto m-4"
+                  />
+                  {question.options[0]}
+                </label>
+                <label htmlFor={`bottom-radio-${question.id}`}>
+                  <input
+                    type="radio"
+                    name={`answer-${question.id}`}
+                    id={`bottom-radio-${question.id}`}
+                    className="w-auto m-4"
+                  />
+                  {question.options[1]}
+                </label>
+              </li>
+            );
+          })}
         </ul>
-        <button type="submit">검사 완료</button>
+        <button className="mb-14">검사 완료</button>
       </form>
     </div>
   );
