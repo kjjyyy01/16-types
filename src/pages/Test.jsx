ba8769f74e22205e1127d81a6c85065f3efe213f
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-23 19:09:39 +0900

    #13 - feature: tanstackQuery 적용

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import { questions } from "../data/questions";
 import { Link } from "react-router-dom";
+import { mbtiDescriptions, mbtiResult } from "../utils/mbtiCalculator";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { addResult } from "../api/testResult";
 
 const Test = () => {
   const [result, setResult] = useState(null);
 
+  const queryClient = useQueryClient();
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    mutate({ mbti: mbtiResult });
   };
+
+  const { mutate } = useMutation({
+    mutationFn: addResult,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["testResults"]);
+    },
+  });
+
   return (
     <>
       {!result ? (
@@ -15,44 +30,37 @@ const Test = () => {
           <form onSubmit={onSubmitHandler} className="flex flex-col justify-center items-center gap-10 mt-14">
             <h1 className="text-5xl font-bold">MBTI 유형 검사</h1>
             <ul className="w-2/5">
-              {questions.map((question) => {
-                return (
-                  <li key={question.id} className="flex flex-col mb-5">
-                    <p className="font-semibold">{question.question}</p>
-                    <label htmlFor={`top-radio-${question.id}`}>
+              {questions.map((question) => (
+                <li key={question.id} className="flex flex-col mb-5">
+                  <p className="font-semibold">{question.question}</p>
+                  {question.options.map((option, index) => (
+                    <label key={index} htmlFor={`radio-${question.id}-${index}`}>
                       <input
                         type="radio"
                         name={`answer-${question.id}`}
-                        id={`top-radio-${question.id}`}
+                        id={`radio-${question.id}-${index}`}
                         className="w-auto m-4"
                       />
-                      {question.options[0]}
+                      {option}
                     </label>
-                    <label htmlFor={`bottom-radio-${question.id}`}>
-                      <input
-                        type="radio"
-                        name={`answer-${question.id}`}
-                        id={`bottom-radio-${question.id}`}
-                        className="w-auto m-4"
-                      />
-                      {question.options[1]}
-                    </label>
-                  </li>
-                );
-              })}
+                  ))}
+                </li>
+              ))}
             </ul>
-            <button onClick={() => setResult((prev) => !prev)} className="mb-14">
+            <button type="button" onClick={() => setResult((prev) => !prev)} className=" mb-14">
               검사 완료
             </button>
           </form>
         </main>
       ) : (
         <main className="w-full flex justify-center items-center">
-          <div className="h-full w-1/3 mt-14 flex flex-col justify-start bg-card shadow-[5px_5px_5px_#DDBC89] rounded-md ">
-            <h1 className="text-5xl font-bold m-10">검사결과: ISTJ </h1>
-            <p className="ml-10 mb-10">당신의 성격은 ~~</p>
-            <Link to={"/results"}>
-              <button className="w-1/3">모든 결과 보기</button>
+          <div className="h-full w-1/3 mt-14 p-10 flex flex-col justify-start bg-card shadow-[5px_5px_5px_#DDBC89] rounded-md ">
+            <h1 className="text-5xl font-bold mb-10">검사결과: {mbtiResult}</h1>
+            <p className="mb-10">{mbtiDescriptions[mbtiResult]}</p>
+            <Link to={"/results"} className="flex justify-center">
+              <button type="button" className="w-1/3">
+                모든 결과 보기
+              </button>
             </Link>
           </div>
         </main>
