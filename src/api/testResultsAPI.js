44292664a87dc9549f5825819470e6f2331daeff
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-24 16:25:25 +0900

    #16 - Structure: 파일명 변경

---
+++ src/api/testResultsAPI.js
@@ -0,0 +1,35 @@
+import { jsonServerAPI } from "../axios/api";
+
+export const addResult = async (testResult) => {
+  try {
+    await jsonServerAPI.post("/testResults", testResult);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const removeResult = async (id) => {
+  try {
+    await jsonServerAPI.delete(`/testResults/${id}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getResult = async () => {
+  try {
+    const response = await jsonServerAPI.get("/testResults");
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const updateProfile = async () => {
+  try {
+    const response = await jsonServerAPI.patch(`/testResults`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
