6241b134faf5b2f7926d2040ba43aad195ae0d85
Jongyeon Kim <tyjysong0712@gmail.com>
2025-02-24 16:26:05 +0900

    #16 - feature: 닉네임 수정 기능 추가

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,50 @@
+import { useEffect, useState } from "react";
+import { authAPI } from "../axios/api";
+
 const Profile = () => {
+  const [nickname, setNickname] = useState("");
+
+  useEffect(() => {
+    const getLoginData = async () => {
+      const accessToken = localStorage.getItem("accessToken");
+      const { data } = await authAPI.get("/user", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
+      setNickname(data.nickname);
+    };
+    getLoginData();
+  }, []);
+
+  const updateNickname = async (e) => {
+    e.preventDefault();
+    const accessToken = localStorage.getItem("accessToken");
+    const { data } = await authAPI.patch(
+      "/profile",
+      {
+        avatar: [],
+        nickname: nickname,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    setNickname(data.nickname);
+    alert("닉네임 수정이 완료되었습니다.");
+  };
+
   return (
     <div>
       <h1>프로필 수정</h1>
-      <form>
-        <input type="text" pattern="^{2,6}$" required />
+      <form onSubmit={updateNickname}>
+        <input
+          type="text"
+          pattern="^[a-z0-9]{2,10}$"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+          required
+        />
         <button>수정 완료</button>
       </form>
     </div>
